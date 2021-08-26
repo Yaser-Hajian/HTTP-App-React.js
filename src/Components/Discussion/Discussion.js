f632c8ccfc084c3a0f4fbdbefd0eef2ddeb8adfc
Yaser <mohammadyaser1379@gmail.com>
2021-08-26 10:54:35 +0430

    get comments from api using axios

--- src/Components/Discussion/Discussion.js
+++ src/Components/Discussion/Discussion.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Comment from "../Comment/Comment";
 import FullComment from "../FullComment/FullComment";
 import NewComment from "../NewComment/NewComment";
 import styles from './DiscussionStyle.module.css'
+import axios from "axios";
 const Discussion = () => {
+    const [comments , setComments] = useState(null);
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/comments")
+            .then((response)=>{
+                setComments(response.data.slice(0,4))})
+            .catch((error)=>{
+                console.log(error)})
+    },[])
     return (
         <div className={styles.container}>
             <div className={styles.comments}>
-                <Comment
-                    name={"yaser"}
-                    email={"yaser email"}
-                />
-                <Comment
-                    name={"ali"}
-                    email={"ali email"}
-                />
-                <Comment
-                    name={"hasan"}
-                    email={"hasan email"}
-                />
+                {
+                    comments ?
+                        (comments.map(c => {
+                          return  <Comment name={c.name} email={c.email} key={c.id}/>
+                        }))
+                        :
+                        (<p>loading comments ...</p>)
+                }
             </div>
             <div>
                 <FullComment
--- src/Components/NewComment/NewComment.js
+++ src/Components/NewComment/NewComment.js
@@ -4,9 +4,9 @@ const NewComment = () => {
     return (
         <div className={styles.container}>
             <form>
-                <span>name: <input type="text" placeholder={"name"}/></span>
-                <span>email: <input type="text" placeholder={"email"}/></span>
-                <span>context: <input type="text" placeholder={"context"}/></span>
+                <span><label>name:</label> <input type="text" placeholder={"name"}/></span>
+                <span><label>email:</label> <input type="email" placeholder={"email"}/></span>
+                <span><label>context:</label> <input type="text" placeholder={"context"}/></span>
                 <button className={styles.add} type={"submit"}>ADD</button>
             </form>
 
