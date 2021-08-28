facf7bb862b9f2691513579d470e9773081cc87b
Yaser <mohammadyaser1379@gmail.com>
2021-08-28 11:57:47 +0430

    NewComment component completed and new comment will post using axios

--- src/Components/NewComment/NewComment.js
+++ src/Components/NewComment/NewComment.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './NewCommentStyle.module.css'
+import axios from "axios";
+
 const NewComment = () => {
+    const [newComment, setNewComment] = useState({name: "", email: "", content: ""});
+    const changeHandler = (event)=>{
+        setNewComment({...newComment , [event.target.name] : event.target.value});
+    }
+    const submitHandler = (event)=>{
+        event.preventDefault();
+        if (newComment.name === ""){
+            alert("please enter your name");
+        }else if (newComment.email === ""){
+            alert("please enter your email");
+        }else if (newComment.content === ""){
+            alert("please enter the content on new comment");
+        }else {
+            axios.post("https://jsonplaceholder.typicode.com/comments" , newComment)
+                .then(response => console.log(response))
+                .catch(error => console.log(error))
+            setNewComment({name: "" ,email: "",content: ""})
+        }
+    }
     return (
         <div className={styles.container}>
-            <form>
-                <span><label>name:</label> <input type="text" placeholder={"name"}/></span>
-                <span><label>email:</label> <input type="email" placeholder={"email"}/></span>
-                <span><label>context:</label> <input type="text" placeholder={"context"}/></span>
+            <form onSubmit={submitHandler}>
+                <span><label>name:</label>
+                    <input type="text"
+                           placeholder={"name"}
+                           name={"name"}
+                           onChange={changeHandler}
+                           value={newComment.name}/>
+                </span>
+                <span><label>email:</label>
+                    <input type="email"
+                           placeholder={"email"}
+                           name={"email"}
+                           onChange={changeHandler}
+                           value={newComment.email}/>
+                </span>
+                <span><label>context:</label>
+                    <input type="text"
+                           placeholder={"context"}
+                           name={"content"}
+                           onChange={changeHandler}
+                           value={newComment.content}/>
+                </span>
                 <button className={styles.add} type={"submit"}>ADD</button>
             </form>
 
